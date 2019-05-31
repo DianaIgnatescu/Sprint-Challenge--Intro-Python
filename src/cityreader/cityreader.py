358ed39425df38fd688b430b39f0d149736c078f
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-31 09:21:00 +0100

    MVP: Complete requirements in cityreader.py

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,3 +1,4 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
@@ -14,14 +15,36 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+
+
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        print(type(lat))
+        # float() is necessary because values from files come in as strings &
+        # the tests expect lat and lon to be floating point numbers
+        self.lat = float(lat)
+        self.lon = float(lon)
+
+    def __str__(self):
+        return f"{self.name}, {self.lat}, {self.lon}"
+
+
 cities = []
 
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
+    with open('cities.csv', newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        # For each city record, create a new City instance and add it to the
+        # `cities` list
+        for row in reader:
+            # Use correct fieldnames from cities.csv
+            cities.append(City(row["city"], row["lat"], row["lng"]))
+
     return cities
+
 
 cityreader(cities)
 
@@ -61,11 +84,11 @@ for c in cities:
 # TODO Get latitude and longitude values from the user
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
+    # within will hold the cities that fall within the specified region
+    within = []
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+    # TODO Ensure that the lat and lon valuse are all floats
+    # Go through each city and check to see if it falls within
+    # the specified coordinates.
 
-  return within
+    return within
